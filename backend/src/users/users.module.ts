1fef9a1cb2c62d6ac1276713712d065918c77caf
Hanna Salameh <hannasalameh9@gmail.com>
2026-01-19 02:21:55 +0200

    add comment for secret key

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -4,11 +4,11 @@ import { JwtModule } from '@nestjs/jwt';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './users.entity';
-
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     JwtModule.register({
+      //i should not add the hardcoded secret key here supersecretkey , but just for making sure that when start the app everything works thanks
       secret: process.env.JWT_SECRET || 'supersecretkey',
       signOptions: { expiresIn: '1h' },
     }),
